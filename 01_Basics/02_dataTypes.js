03daedea68bfee1deadf2fcbd178d58c191d1c09
Shubham Pandey <shubhampandey567@gmail.com>
2024-03-06 18:07:03 +0000

    datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+//old javaScipt didn't have class, module, arrow functions
+"use strict";//javascript engin treats all JS code as newer version of JS
+//alert( 3+3) // we are using nodejs not browser hence alert is undefined here
+
+
+let name = "TestName"
+let age = 18
+let isLoggedIn = false
+
+console.log(typeof age)//to know type of an object
+
+//premitive data types
+//number: 2^53
+//bigint
+//string: ""
+//bool: true/false
+//null: standalone value
+//undefined: value not yet assigned
+//symbol: unique
+
+//object
