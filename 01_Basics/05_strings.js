e9005f74315aafac702b437811b1422b011ac5a9
Shubham Pandey <shubhampandey567@gmail.com>
2024-03-07 18:01:51 +0000

    string

---
+++ 01_Basics/05_strings.js
@@ -0,0 +1,8 @@
+const name = "TestName"
+const repoCount = 50
+
+const a = String("laskdfj")
+
+console.log(`Hello my name is ${name} and my repo is count is ${repoCount}`)
+console.log(name[0])
+console.log(a)
